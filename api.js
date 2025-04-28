6c8a573102097f45b6f834995ebc81bdb5fd8a40
parsashaabani3@gmail.com <parsashaabani3@gmail.com>
2025-04-29 01:11:57 +0330

    add model

--- api.js
+++ api.js
@@ -11,7 +11,117 @@ const validApiKeys = [
 ];
 
 // In-memory courses store (in production, use a database)
-let courses = [];
+let courses = [
+  {
+    id: 1,
+    titleImage:
+      "https://www.rasa-ai.com/wp-content/uploads/2021/10/python-course.jpg",
+    titleText: "دورۀ متخصص برنامه نویسی",
+    publisherName: "پارسا شعبانی",
+    publisherImage: "https://avatars.githubusercontent.com/u/122119546?v=4",
+    time: "۳۰ ساعت",
+    meetsCount: "۳۰ جلسه",
+    publishTime: "12 بهمن 1403",
+  },
+  {
+    id: 2,
+    titleImage: "https://www.karlancer.com/media/worksample/7nwmm5r1nwr3.jpg",
+    titleText: "دورۀ پیشرفته پایتون",
+    publisherName: "پارسا شعبانی",
+    publisherImage: "https://avatars.githubusercontent.com/u/122119546?v=4",
+    time: "۴۰ ساعت",
+    meetsCount: "۳۵ جلسه",
+    publishTime: "18 اسفند 1403",
+  },
+  {
+    id: 3,
+    titleImage:
+      "https://img.freepik.com/free-photo/programming-background-collage_23-2149901770.jpg",
+    titleText: "آموزش فریمورک Django",
+    publisherName: "پارسا شعبانی",
+    publisherImage: "https://avatars.githubusercontent.com/u/122119546?v=4",
+    time: "۲۵ ساعت",
+    meetsCount: "۲۰ جلسه",
+    publishTime: "5 فروردین 1404",
+  },
+  {
+    id: 4,
+    titleImage:
+      "https://darsman.com/blog/wp-content/uploads/2021/06/python-programming-with-phone.jpg",
+    titleText: "آموزش پروژه محور Python",
+    publisherName: "پارسا شعبانی",
+    publisherImage: "https://avatars.githubusercontent.com/u/122119546?v=4",
+    time: "۵۰ ساعت",
+    meetsCount: "۴۵ جلسه",
+    publishTime: "25 فروردین 1404",
+  },
+  {
+    id: 5,
+    titleImage:
+      "https://www.shutterstock.com/image-photo/backend-development-coding-software-engineering-programming-260nw-1932442547.jpg",
+    titleText: "متخصص بک اند Python",
+    publisherName: "پارسا شعبانی",
+    publisherImage: "https://avatars.githubusercontent.com/u/122119546?v=4",
+    time: "۳۵ ساعت",
+    meetsCount: "۳۲ جلسه",
+    publishTime: "10 اردیبهشت 1404",
+  },
+  {
+    id: 6,
+    titleImage:
+      "https://www.istockphoto.com/photo/python-programming-language-concept-with-computer-code-and-laptop-gm1212256253-352913190",
+    titleText: "برنامه نویسی وب با Python",
+    publisherName: "پارسا شعبانی",
+    publisherImage: "https://avatars.githubusercontent.com/u/122119546?v=4",
+    time: "۴۵ ساعت",
+    meetsCount: "۴۰ جلسه",
+    publishTime: "20 اردیبهشت 1404",
+  },
+  {
+    id: 7,
+    titleImage:
+      "https://img.freepik.com/free-photo/html-css-collage-concept_23-2149409985.jpg",
+    titleText: "طراحی وب پیشرفته با Django",
+    publisherName: "پارسا شعبانی",
+    publisherImage: "https://avatars.githubusercontent.com/u/122119546?v=4",
+    time: "۲۰ ساعت",
+    meetsCount: "۱۵ جلسه",
+    publishTime: "1 خرداد 1404",
+  },
+  {
+    id: 8,
+    titleImage:
+      "https://img.freepik.com/free-vector/flat-composition-with-programmer-testing-programs-illustration_1284-63745.jpg",
+    titleText: "آموزش تست نویسی در Python",
+    publisherName: "پارسا شعبانی",
+    publisherImage: "https://avatars.githubusercontent.com/u/122119546?v=4",
+    time: "۱۵ ساعت",
+    meetsCount: "۱۲ جلسه",
+    publishTime: "10 خرداد 1404",
+  },
+  {
+    id: 9,
+    titleImage:
+      "https://img.freepik.com/free-photo/database-concept-with-person-working-laptop_23-2150147655.jpg",
+    titleText: "دیتابیس و Python",
+    publisherName: "پارسا شعبانی",
+    publisherImage: "https://avatars.githubusercontent.com/u/122119546?v=4",
+    time: "۲۰ ساعت",
+    meetsCount: "۱۸ جلسه",
+    publishTime: "20 خرداد 1404",
+  },
+  {
+    id: 10,
+    titleImage:
+      "https://www.dreamstime.com/photos-images/python-networking-security.html",
+    titleText: "دوره کامل امنیت با Python",
+    publisherName: "پارسا شعبانی",
+    publisherImage: "https://avatars.githubusercontent.com/u/122119546?v=4",
+    time: "۶۰ ساعت",
+    meetsCount: "۵۵ جلسه",
+    publishTime: "30 خرداد 1404",
+  },
+];
 let idCounter = 2; // Start from 2 since we have one course already
 
 // Middleware to verify API key
@@ -54,7 +164,7 @@ app.post("/api/courses", authenticateApiKey, (req, res) => {
     meetsCount,
     publishTime,
   } = req.body;
-  
+
   const course = {
     id: idCounter++,
     titleImage:
